754d0cf892f389234dbfaa36c547f759e373b0e1
k-ganda <k.ganda@alustudent.com>
2023-09-10 20:38:20 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,14 @@
-#!/usr/bin/python3
-"""Defines BaseModel class."""
+#!usr/bin/python3
+"""Defines all common attributes/methods for other classes."""
 
 import models
 import uuid
 from datetime import datetime
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """
-        Class Base
-        Defines all common attributes/methods for other classes
-        Attr :
-                id: string - assigned with an uuid when an instance is created
-                created_at: datetime - assigned with the current datetime
-                when an instance is created
-
-                updated_at: datetime - assigned with the current datetime
-                when an instance is created.
-                It will be updated every time the object change.
-    """
+    """Class from which the future classes will be derived"""
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
@@ -36,22 +26,19 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """Set updated_at with current datetime."""
-        self.updated_at = datetime.now()
+        """Updates updated_at with current time when instance is changed"""
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
-        """Return dictionary of BaseModel instance.
-
-        Includes key/value pair __class__.
-        """
+        """Returns a dictionary containing all keys/values of __dict__"""
         rdict = self.__dict__.copy()
+        rdict["__class__"] = self.__class__.__name__
         rdict["created_at"] = self.created_at.isoformat()
         rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
         return rdict
 
     def __str__(self):
-        """Return print/str representation of BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """Returns a string representation of the instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
