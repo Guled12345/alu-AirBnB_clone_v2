182bc01e5bd8066ece9f5fe50514d7ec6ac22514
k-ganda <k.ganda@alustudent.com>
2023-09-10 12:40:20 +0200

    Update command interpreter

--- console.py
+++ console.py
@@ -1,51 +1,112 @@
 #!/usr/bin/python3
-"""Entry point of Airbnb console"""
+"""Defines AirBnb console."""
 import cmd
 import re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
-    """Defines HBNB Command Interpreter.
-    
+    """Defines command interpreter.
+
     Attributes:
-        prompt: prompt string
+        prompt (str): The command prompt.
     """
-    
+
     prompt = "(hbnb) "
-    
+    __all_classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+
     def emptyline(self):
-        """Does nothing when an empty line is entered."""
+        """
+        Called when an empty line is entered.
+        If this method is not overridden like this,
+        it repeats the last nonempty
+        command entered.
+        It should not be empty_line, so ignore the typo warnings.
+        (see: https://github.com/python/cpython/blob/3.11/Lib/cmd.py)
+        """
         pass
-    
+
+    def default(self, arg):
+        """This is the default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
     def do_quit(self, arg):
-        """Quit command to exit the program."""
+        """Quit command -> exit the program."""
         return True
-    
+
     def do_EOF(self, arg):
-        """EOF command to exit the program."""
+        """EOF signal -> exit the program."""
         print("")
         return True
-    
-    def do_create(self, arg):
+
+    def do_create(self, args):
         """
-        Creates a new instance of BaseModel,
-        saves it (to the JSON file) and prints the id.
-        Usage: create <class name>
-        """
-        # if class name is missing,
-        # print ** class name missing **  (ex: $ create)
+            Creates a new instance of a class,
+            saves it (to the JSON file) and prints the id.
+            Usage: create <class_name>
+       """
+        # If the class name is missing,
+        # print ** class name missing ** (ex: $ create)
         if len(args) < 1:
             print("** class name missing **")
             return
-        # if class name doesn't exist,
+        # If the class name doesn’t exist,
         # print ** class doesn't exist ** (ex: $ create MyModel)
-        
-        # converting the args to a list
+
+        # convert the args to a list
         args = args.split()
-        
+
         # the 1st element of the list is the class name
         class_name = args[0]
         if class_name not in self.__all_classes:
@@ -57,7 +118,7 @@ class HBNBCommand(cmd.Cmd):
         new_object.save()
         print(new_object.id)
         storage.save()
-        
+
     def do_show(self, args):
         """Usage: to show <class> <id> or <class>.show(<id>)
         Display string representation of a class instance of given id.
@@ -81,7 +142,7 @@ class HBNBCommand(cmd.Cmd):
             return
         else:
             print(objdict[object_key])
-    
+
     def do_destroy(self, args):
         """Usage: to destroy <class> <id> or <class>.destroy(<id>)
         Delete class instance of given id."""
@@ -107,13 +168,11 @@ class HBNBCommand(cmd.Cmd):
         else:
             del all_objects[object_key]
             storage.save()
-            
+
     def do_all(self, args):
-        """
-        Usage: all or all <class> or <class>.all()
+        """Usage: all or all <class> or <class>.all()
         Display string representations of all instances of given class
-        If no class is specified, display all instantiated objects.
-        """
+        If no class is specified, display all instantiated objects."""
 
         arg_list = args.split()
         all_objects = storage.all()
@@ -210,8 +269,8 @@ class HBNBCommand(cmd.Cmd):
 
         storage.save()
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     """
     Repeatedly issue a prompt, accept input, parse an initial prefix
     off the received input, and dispatch to action methods, passing them
